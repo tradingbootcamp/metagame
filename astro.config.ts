9f44dc7927a28044568aea02a33084178ad956d0
Ricki Heicklen <ricki.heicklen@gmail.com>
2024-12-09 02:02:07 -0500

    trying to unfuck the icon problem

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import icon from "astro-icon";
 
 import react from "@astrojs/react";
 
 export default defineConfig({
   site: "https://metagames.games",
-  integrations: [tailwind(), icon(), react({
+  integrations: [tailwind(), react({
     include: ['**/react/*'],
   })],
   vite: {
