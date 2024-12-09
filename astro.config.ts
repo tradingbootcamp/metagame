7a1b9ba5c13094d250fd56dc6bc6bad096dfb646
Ricki Heicklen <ricki.heicklen@gmail.com>
2024-12-09 01:28:30 -0500

    making changes for github pages publishing

--- astro.config.ts
+++ astro.config.ts
@@ -5,8 +5,7 @@ import icon from "astro-icon";
 import react from "@astrojs/react";
 
 export default defineConfig({
-  site: "https://RickiHeicklen.github.io",
-  base: "/",
+  site: "https://metagames.games",
   integrations: [tailwind(), icon(), react({
     include: ['**/react/*'],
   })],
