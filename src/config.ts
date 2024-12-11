d5a8fca8c83d8319ccfb50dcf701acbe01c443b3
Ricki Heicklen <ricki.heicklen@gmail.com>
2024-12-11 16:46:09 -0500

    adding interest form where ticket buying would usually go

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const TICKET_URL = import.meta.env.PUBLIC_TICKET_URL ?? '';
+export const TICKET_URL = import.meta.env.PUBLIC_TICKET_URL ?? 'https://bit.ly/metagame-interest';
 export const CALL_FOR_SPEAKERS = import.meta.env.PUBLIC_CALL_FOR_SPEAKERS ?? '';
 export const CALL_FOR_VOLUNTEERS = import.meta.env.PUBLIC_CALL_FOR_VOLUNTEERS ?? '';
 export const CALL_FOR_SPONSORS = import.meta.env.PUBLIC_CALL_FOR_SPONSORS ?? '';
