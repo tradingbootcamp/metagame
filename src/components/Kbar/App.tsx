502758090ec254cf2d8d4df750a0cbbf64f1d998
Ricki Heicklen <ricki.heicklen@gmail.com>
2024-12-18 17:51:56 -0500

    kbar intiial behavior

---
+++ src/components/Kbar/App.tsx
@@ -0,0 +1,50 @@
+// app.tsx
+import {
+  KBarProvider,
+  KBarPortal,
+  KBarPositioner,
+  KBarAnimator,
+  KBarSearch,
+  KBarResults,
+  useMatches,
+  NO_GROUP
+} from "kbar";
+import { actions } from "../../config/kbarActions";
+
+function RenderResults() {
+  const { results } = useMatches();
+    
+    return (
+      <KBarResults
+      items={results}
+      onRender={({ item, active }) =>
+      typeof item === "string" ? (
+        <div>{item}</div>
+        ) : (
+          <div
+          style={{
+            background: active ? "#eee" : "transparent",
+          }}
+          >
+          {item.name}
+          </div>
+          )
+        }
+      />
+    );
+}
+
+export function KbarApp({ children }: { children: React.ReactNode }) {
+  return (
+    <KBarProvider actions={actions}>
+      <KBarPortal>
+        <KBarPositioner>
+          <KBarAnimator>
+            <RenderResults />
+          </KBarAnimator>
+        </KBarPositioner>
+      </KBarPortal>
+      {children}
+    </KBarProvider>
+  );
+}
