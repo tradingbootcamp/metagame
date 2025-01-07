0219eace626d332ba273140e3ad5deaa3f0b5b7a
Ricki Heicklen <ricki.heicklen@gmail.com>
2025-01-06 20:10:29 -0500

    adding ticket payment links

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,9 @@
-export const TICKET_URL = import.meta.env.PUBLIC_TICKET_URL ?? 'https://bit.ly/metagame-interest';
+export const INTEREST_FORM_URL = import.meta.env.PUBLIC_INTEREST_FORM_URL ?? 'https://bit.ly/metagame-interest';
+export const TICKET_EARLY_BIRD_URL = import.meta.env.PUBLIC_TICKET_EARLY_BIRD_URL ?? 'https://bit.ly/metagame-early-bird-ticket';
+export const TICKET_REGULAR_URL = import.meta.env.PUBLIC_TICKET_REGULAR_URL ?? '';
+export const TICKET_VOLUNTEER_URL = import.meta.env.PUBLIC_TICKET_VOLUNTEER_URL ?? 'https://bit.ly/metagame-volunteer-ticket';
+export const TICKET_SUPPORTER_URL = import.meta.env.PUBLIC_TICKET_SUPPORTER_URL ?? 'bit.ly/metagame-supporter-ticket';
+
 export const CALL_FOR_SPEAKERS = import.meta.env.PUBLIC_CALL_FOR_SPEAKERS ?? '';
 export const CALL_FOR_VOLUNTEERS = import.meta.env.PUBLIC_CALL_FOR_VOLUNTEERS ?? '';
 export const CALL_FOR_SPONSORS = import.meta.env.PUBLIC_CALL_FOR_SPONSORS ?? '';
