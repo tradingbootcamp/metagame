32e1b0d07fa22fd3bda82eee47b0aebd2850bcbd
Ricki Heicklen <ricki.heicklen@gmail.com>
2025-05-27 08:56:45 -0700

    fixing airtable link

--- src/config.ts
+++ src/config.ts
@@ -4,9 +4,9 @@ export const TICKET_REGULAR_URL = import.meta.env.PUBLIC_TICKET_REGULAR_URL ?? '
 export const TICKET_VOLUNTEER_URL = import.meta.env.PUBLIC_TICKET_VOLUNTEER_URL ?? 'https://bit.ly/metagame-volunteer-ticket';
 export const TICKET_SUPPORTER_URL = import.meta.env.PUBLIC_TICKET_SUPPORTER_URL ?? 'https://bit.ly/metagame-supporter-ticket';
 
-export const CALL_FOR_SPEAKERS = import.meta.env.PUBLIC_CALL_FOR_SPEAKERS ?? '';
-export const CALL_FOR_VOLUNTEERS = import.meta.env.PUBLIC_CALL_FOR_VOLUNTEERS ?? '';
-export const CALL_FOR_SPONSORS = import.meta.env.PUBLIC_CALL_FOR_SPONSORS ?? '';
+export const CALL_FOR_SPEAKERS = import.meta.env.PUBLIC_CALL_FOR_SPEAKERS ?? 'https://airtable.com/appTvPARUssZp4qiB/pagqYAgxKlTdRbS6n/form';
+export const CALL_FOR_VOLUNTEERS = import.meta.env.PUBLIC_CALL_FOR_VOLUNTEERS ?? 'https://airtable.com/appTvPARUssZp4qiB/pagqYAgxKlTdRbS6n/form';
+export const CALL_FOR_SPONSORS = import.meta.env.PUBLIC_CALL_FOR_SPONSORS ?? 'https://airtable.com/appTvPARUssZp4qiB/pagqYAgxKlTdRbS6n/form';
 export const SOCIAL_LINKS = {
     DISCORD: 'https://discord.gg/8pxvc9p4Ss',
     TWITTER: 'https://x.com/tradegal_'
