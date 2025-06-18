3c4bc8ffec5a96651e9870a4694bc2c2391fc59e
Ricki Heicklen <ricki.heicklen@gmail.com>
2025-06-18 18:02:58 -0400

    upgrading astro and vercel versions

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 
 export default defineConfig({
   site: "https://metagames.games",
-  output: "server",
-  adapter: vercel(),
+  adapter: vercel({}),
   integrations: [
     tailwind(), 
     react({
