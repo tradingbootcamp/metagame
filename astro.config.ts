b75690d9414bcdce86d18c09612b35b36846669b
Ricki Heicklen <ricki.heicklen@gmail.com>
2025-06-18 17:38:08 -0400

    fixing vercel adapter because we are using the server instead of hosting a static site

--- astro.config.ts
+++ astro.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import node from "@astrojs/node";
+import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
   site: "https://metagames.games",
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  }),
+  adapter: vercel({}),
   integrations: [
     tailwind(), 
     react({
