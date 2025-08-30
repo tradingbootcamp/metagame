2b78bcb02410d7eae2c83ddc7556284ce4cac30a
Brian Smiley <briantsmiley42@gmail.com>
2025-08-30 11:13:09 -0700

    buttons and tooltips a little more visible

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -11,7 +11,7 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          'bg-bg-tertiary border border-border-secondary text-primary shadow-xs hover:bg-bg-tertiary/80',
+          'bg-blue-700 border border-border-secondary text-primary shadow-xs hover:bg-blue-800',
         destructive:
           'bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60',
         outline:
