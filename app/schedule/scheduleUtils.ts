557f1960e65ec7b241640ee122d802d027e98789
Brian Smiley <briantsmiley42@gmail.com>
2025-09-03 13:37:42 -0700

    fixes time date strings on google calendar links

--- app/schedule/scheduleUtils.ts
+++ app/schedule/scheduleUtils.ts
@@ -10,7 +10,22 @@ export const gCalLinkFromSession = (session: DbFullSession) => {
   const title = `Metagame: ${session.title || 'Untitled Session'}`
   const hostsString = dbGetHostsFromSession(session).join(', ')
   const detailsString = `${locationString}\nHosts: ${hostsString}\n\n=================\n\n${session.description + '\n\n=================\n\n' || ''}${sessionLink(session.id)}`
-  return `https://www.google.com/calendar/render?action=TEMPLATE&text=${encodeURIComponent(title)}&dates=${session.start_time}/${session.end_time}&location=${encodeURIComponent(addressString)}&details=${encodeURIComponent(detailsString)}`
+  const toCalendarString = (date: Date) => {
+    const year = date.getUTCFullYear().toString().padStart(4, '0')
+    const month = (date.getUTCMonth() + 1).toString().padStart(2, '0')
+    const day = date.getUTCDate().toString().padStart(2, '0')
+    const hours = date.getUTCHours().toString().padStart(2, '0')
+    const minutes = date.getUTCMinutes().toString().padStart(2, '0')
+    const seconds = date.getUTCSeconds().toString().padStart(2, '0')
+    console.log(year, month, day, hours, minutes, seconds)
+    return `${year}${month}${day}T${hours}${minutes}${seconds}Z`
+  }
+  if (session.start_time && session.end_time) {
+    const startTimeString = toCalendarString(new Date(session.start_time))
+    const endTimeString = toCalendarString(new Date(session.end_time))
+    return `https://www.google.com/calendar/render?action=TEMPLATE&text=${encodeURIComponent(title)}&dates=${startTimeString}/${endTimeString}&location=${encodeURIComponent(addressString)}&details=${encodeURIComponent(detailsString)}`
+  }
+  return `https://www.google.com/calendar/render?action=TEMPLATE&text=${encodeURIComponent(title)}&location=${encodeURIComponent(addressString)}&details=${encodeURIComponent(detailsString)}`
 }
 
 export const sessionLink = (sessionId: string) => {
