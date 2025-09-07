df3c477b0a4c648de800d11b9b27428197510e08
Brian Smiley <briantsmiley42@gmail.com>
2025-09-07 02:14:48 -0700

    adds tilt shine effect to portrait border on cards

--- app/profile/Profile.tsx
+++ app/profile/Profile.tsx
@@ -238,6 +238,7 @@ export default function Profile() {
                     userId={currentUser.id}
                     asProfile
                     tiltFactor={2.5}
+                    gleamFollowsTilt
                   />
                 )}
               </div>
