8f614c618b54ec17dabc3fa1da017353143693b9
Brian Smiley <briantsmiley42@gmail.com>
2025-09-06 18:35:32 -0700

    restyles highlights section

--- components/sections/home/Highlights.tsx
+++ components/sections/home/Highlights.tsx
@@ -6,10 +6,9 @@ import { IoCalendarNumberSharp } from 'react-icons/io5'
 import { MoonIcon } from 'lucide-react'
 import Link from 'next/link'
 
-import { cn } from '@/utils/cn'
 import { URLS } from '@/utils/urls'
 
-import { buttonVariants } from '@/components/ui/button'
+import { Button } from '@/components/Button'
 import { Separator } from '@/components/ui/separator'
 
 import { getSessionById } from '@/app/actions/db/sessions'
@@ -25,8 +24,8 @@ export default async function Highlights() {
       <div className="mb-4 self-center text-2xl font-bold text-secondary-200">
         Highlight Events
       </div>
-      <div className="flex flex-col gap-4">
-        <div className="self-start">
+      <div className="flex flex-col gap-8">
+        <div className="flex flex-col gap-2 self-start">
           <span className="text-xl font-bold">
             The Metagame <span className="text-orange-500">M</span>
             <span className="text-purple-500">e</span>
@@ -48,8 +47,9 @@ export default async function Highlights() {
             </Link>
           </p>
         </div>
+        <Separator />
         <div className="flex flex-col gap-4 self-end">
-          <div className="flex flex-col gap-2">
+          <div className="flex flex-col">
             <span className="flex items-center gap-2 text-xl font-bold">
               The Night Market <MoonIcon />
             </span>
@@ -87,9 +87,9 @@ export default async function Highlights() {
             </strong>
             ! Come and find booths based around:
           </p>
-          <ul className="grid grid-cols-3 justify-center gap-4">
+          <ul className="grid grid-cols-1 justify-center gap-4 sm:grid-cols-3">
             <li className="flex flex-col items-center">
-              <strong className="text-center">Games and Puzzles</strong>
+              <strong className="sm:text-center">Games and Puzzles</strong>
               <Separator />
               <span>
                 Indie TTRPG / jigsaw puzzle / 5-dimensional rubik&apos;s cube /
@@ -97,7 +97,7 @@ export default async function Highlights() {
               </span>
             </li>
             <li className="flex flex-col items-center">
-              <strong className="text-center">Other Physical Stuff</strong>
+              <strong className="sm:text-center">Other Physical Stuff</strong>
               <Separator />
               <span>
                 Arts, crafts, foods, books, take-home items that don&apos;t fit
@@ -105,7 +105,7 @@ export default async function Highlights() {
               </span>
             </li>
             <li className="flex flex-col items-center">
-              <strong className="text-center">Experiences</strong>
+              <strong className="sm:text-center">Experiences</strong>
               <Separator />
               <span>
                 Try on a weird VR immersive bodysuit, get your fortune told,
@@ -113,7 +113,7 @@ export default async function Highlights() {
               </span>
             </li>
             <li className="flex flex-col items-center">
-              <strong className="text-center">Job Market</strong>
+              <strong className="sm:text-center">Job Market</strong>
               <Separator />
               <span>
                 People looking to hire, get hired, or otherwise promote their
@@ -122,7 +122,7 @@ export default async function Highlights() {
               </span>
             </li>
             <li className="flex flex-col items-center">
-              <strong className="text-center">Black Market</strong>
+              <strong className="sm:text-center">Black Market</strong>
               <Separator />
               <span>
                 Rights to middle names, tickets to cheat in your next RPG, etc.
@@ -130,27 +130,24 @@ export default async function Highlights() {
               </span>
             </li>
             <li className="flex flex-col items-center">
-              <strong className="text-center">Information Booth</strong>
+              <strong className="sm:text-center">Information Booth</strong>
               <Separator />
               <span>Like a poster session, without the standards</span>
             </li>
             <li className="flex flex-col items-center">
-              <strong className="text-center">Other</strong>
+              <strong className="sm:text-center">Other</strong>
               <Separator />
               <span>
                 Booths too weird to fit into any of the categories above!
               </span>
             </li>
           </ul>
-          <a
-            href={URLS.NIGHT_MARKET_BOOTH}
-            className={cn(
-              buttonVariants({ variant: 'default' }),
-              'w-fit self-center',
-            )}
+          <Button
+            link={URLS.NIGHT_MARKET_BOOTH}
+            className="w-fit self-center text-center"
           >
             Apply for a Vendor Booth!
-          </a>
+          </Button>
         </div>
       </div>
     </section>
