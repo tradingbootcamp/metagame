1f51750650d9610892b73818260e79047ab11109
Brian Smiley <briantsmiley42@gmail.com>
2025-09-12 16:32:03 -0700

    adds sudoku solution images

--- types/database/dbTypeAliases.ts
+++ types/database/dbTypeAliases.ts
@@ -83,3 +83,7 @@ export type DbCouponUpdate = TablesUpdate<'coupons'>
 export type DbCouponEmail = Tables<'coupon_emails'>
 export type DbCouponEmailInsert = TablesInsert<'coupon_emails'>
 export type DbCouponEmailUpdate = TablesUpdate<'coupon_emails'>
+
+export type DbSudoku = Tables<'sudoku'>
+export type DbSudokuInsert = TablesInsert<'sudoku'>
+export type DbSudokuUpdate = TablesUpdate<'sudoku'>
