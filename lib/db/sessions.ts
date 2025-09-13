f3e359934b735574c592c5aab3e399cd8b6d41e5
crthpl <theo@reinsberg.info>
2025-09-13 01:46:29 -0700

    add megagame location column in session migration & change control of territory on win declaration

--- lib/db/sessions.ts
+++ lib/db/sessions.ts
@@ -45,7 +45,7 @@ location:locations!sessions_location_id_fkey (
 megagame_location:megagame_locations!sessions_megagame_location_fkey (
   id,
   name
-)
+),
 card_rewards:celestial_cards(
   *,
   details:session_card_rewards(loser_option)
@@ -178,9 +178,10 @@ export const sessionsService = {
     winningTeam: DbTeamColor
   }) => {
     const supabase = createServiceClient()
+
     const { data: session, error } = await supabase
       .from('sessions')
-      .select('megagame, winning_team')
+      .select('megagame, winning_team, megagame_location')
       .eq('id', sessionId)
       .single()
     if (error) {
@@ -192,6 +193,19 @@ export const sessionsService = {
     if (session.winning_team) {
       throw new Error('Winning team already declared')
     }
+
+    if (session.megagame_location) {
+      const { error: locationError } = await supabase
+        .from('megagame_locations')
+        .update({
+          control: winningTeam,
+        })
+        .eq('id', session.megagame_location)
+      if (locationError) {
+        throw new Error(locationError.message)
+      }
+    }
+
     const { data, error: updateError } = await supabase
       .from('sessions')
       .update({
